1c298e064ddcadcda06a4615513397f5bba58c40
liamk <liam.krenz@menloschool.org>
2024-09-02 17:23:22 -0700

    Finished initial coding of findPath(), running into an infinite recursion bug.

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -25,10 +25,9 @@ public class MokshaPatam {
         landmarks[landmarks.length-1][0] = finalSquare;
         ArrayList<Integer> visited = new ArrayList<>();
 
-        while (location != finalSquare) {
+        ArrayList<Integer> shortestDistance = findPath(0, landmarks, visited);
 
-        }
-        return 0;
+        return (shortestDistance.getFirst() / 6) + 1;
     }
 
     public ArrayList<Integer> findNextMove(int square) {
@@ -37,8 +36,8 @@ public class MokshaPatam {
 
     // Method to combine and sort two 2D integer arrays based off the first index in each array.
     public static int[][] combineArrays(int[][] arr1, int[][] arr2) {
-        int numLandmarks = arr1.length + arr2.length + 1;
-        int[][] arr3 = new int[numLandmarks][2];
+        int numLandmarks = arr1.length + arr2.length;
+        int[][] arr3 = new int[numLandmarks + 2][2];
 
         // Combines the two arrays.
         for (int i = 0; i < arr1.length; i++) {
@@ -60,27 +59,6 @@ public class MokshaPatam {
         return arr3;
     }
 
-    // Returns
-    public static int[] takeLandmark(int location, int landmark, int[][] landmarks, ArrayList<Integer> visited, int steps) {
-        if(visited.contains(landmarks[landmark][1]))
-            return new int[0];
-        for (int i = location; i < landmarks[landmark][0]; i++) {
-            visited.add(i);
-        }
-        if (landmarks[landmark][0] < landmarks[landmark][1]) {
-            ArrayList<Integer> landmarksBetween = findLandmarksBetween(landmark, landmarks, true);
-            if (landmarksBetween == null || landmarksBetween.isEmpty()) {
-                return true;
-            }
-            else {
-                min
-                for (int i = 0; i < landmarksBetween.size(); i++) {
-
-                }
-            }
-        }
-    }
-
     // First index is return array is the length of the path.
     public static ArrayList<Integer> findPath(int location, int[][] landmarks, ArrayList<Integer> visited) {
         if (visited.contains(location)) {
@@ -89,10 +67,10 @@ public class MokshaPatam {
         if (location == landmarks.length - 1) {
             ArrayList<Integer> solution = new ArrayList<>();
             solution.add(location);
+            solution.addFirst(0);
             return solution;
         }
         visited.add(location);
-
 
         // Case if next landmark is a ladder.
         ArrayList<Integer> shortestDistance = new ArrayList<>();
@@ -102,7 +80,27 @@ public class MokshaPatam {
             for (int i = 0; i < paths.length; i++) {
                 paths[i] = findPath(landmarksBetween.get(i), landmarks, visited);
                 if (paths[i] != null) {
-                    paths[i].addFirst(landmarks[landmarksBetween.get(i)][0] - landmarks[location][0]);
+                    paths[i].set(0, paths[i].getFirst() + landmarks[landmarksBetween.get(i)][0] - landmarks[location][0]);
+                }
+
+            }
+            int shortest = 0;
+            for (int i = 0; i < paths.length; i++) {
+                if (paths[i] != null) {
+                    if (paths[shortest].get(0) > paths[i].get(0)) {
+                        shortest = i;
+                    }
+                }
+            }
+            shortestDistance = paths[shortest];
+        } // Case if next landmark is a snake
+        else if (landmarks[location][0] > landmarks[location][1]) {
+            ArrayList<Integer> landmarksBetween = findLandmarksBetween(location, landmarks, false);
+            ArrayList<Integer>[] paths = new ArrayList[landmarksBetween.size()];
+            for (int i = 0; i < paths.length; i++) {
+                paths[i] = findPath(landmarksBetween.get(i), landmarks, visited);
+                if (paths[i] != null) {
+                    paths[i].set(0, paths[i].getFirst() + landmarks[landmarksBetween.get(i)][0] - landmarks[location][0]);
                 }
 
             }
@@ -116,12 +114,15 @@ public class MokshaPatam {
             }
             shortestDistance = paths[shortest];
         }
-        else if (landmarks[location][0] > landmarks[location][1]) {
-            ArrayList<Integer> landmarksBetween = findLandmarksBetween(location, landmarks, false);
 
+        // Case where next landmark is skipped.
+        ArrayList<Integer> solution = findPath(location+1, landmarks, visited);
+        solution.set(0, solution.getFirst() + landmarks[location+1][0] - landmarks[location][0]);
+        visited.remove((Integer) location);
+        if (solution.get(0) < shortestDistance.get(0)) {
+            return solution;
         }
-        // Case if next landmark is a snake.
-        // Case if taking next landmark is not optimal
+        return shortestDistance;
     }
 
     public static ArrayList<Integer> findLandmarksBetween(int landmark, int[][] landmarks, boolean ladder) {
@@ -131,15 +132,21 @@ public class MokshaPatam {
         int i = 0;
         int end = 0;
         if (ladder) {
-            int i = landmark + 1;
-            int end = landmarks[landmark][1];
+            i = landmark + 1;
+            end = landmarks[landmark][1];
         }
         else {
-            while ()
+            int endSnake = landmarks[landmark][1];
+            while (i < landmarks.length) {
+                if (landmarks[i][0] > endSnake) {
+                    break;
+                }
+                i++;
+            }
+            end = landmarks[landmarks.length - 1][0];
         }
 
         ArrayList<Integer> landmarksBetween = new ArrayList<>();
-
 
         while (landmarks[i][0] <= end) {
             if (i == landmarks.length - 1) {
@@ -152,5 +159,4 @@ public class MokshaPatam {
 
         return landmarksBetween;
     }
-
 }
