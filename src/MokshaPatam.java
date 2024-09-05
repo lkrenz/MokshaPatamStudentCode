13c31c23d307f545fe52aa4a814dbe3a076a8406
liamk <liam.krenz@menloschool.org>
2024-09-04 18:36:07 -0700

    Code passes first three tests, need to debug much larger.

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -23,27 +23,39 @@ public class MokshaPatam {
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
         ArrayList<Integer> spaces = new ArrayList<>();
         int location = 0;
-        int finalSpace = boardsize * boardsize - 1;
-        int[] landmarks = makeArray(snakes, ladders);
+        int finalSpace = boardsize - 1;
+        int[] landmarks = makeArray(snakes, ladders, boardsize);
         landmarks[finalSpace - 1] = finalSpace;
         ArrayList<Integer> visited = new ArrayList<>();
         Queue<Integer> toVisit = new LinkedList<>();
-        int[] pathTaken = new int[boardsize];
-
+        int[] pathTaken = new int[finalSpace + 1];
+        System.out.println(Arrays.toString(landmarks));
         while (location != finalSpace) {
-            visited.add(location);
-            for (int i = 0; i < 6; i++) {
-                if (visited.contains(location + i)) {
+            if (location >= 93) {
+                pathTaken[finalSpace] = location;
+                location = finalSpace;
+                break;
+            }
+            for (int i = location + 1; i < location + 7; i++) {
+                if (visited.contains(i)) {
                     continue;
                 }
-                if (landmarks[location + i] != 0) {
-                    toVisit.add(landmarks[location + i]);
-                    pathTaken[landmarks[location + i]] = location;
+                if (landmarks[i] != 0) {
+                    if (!visited.contains(landmarks[i])) {
+                        toVisit.add(landmarks[i]);
+                        visited.add(landmarks[i]);
+                        pathTaken[landmarks[i]] = location;
+                    }
+                    visited.add(i);
                 }
                 else {
-                    toVisit.add(location + i);
-                    pathTaken[location + i] = location;
+                    toVisit.add(i);
+                    visited.add(i);
+                    pathTaken[i] = location;
                 }
+            }
+            if (toVisit.isEmpty()) {
+                return -1;
             }
             location = toVisit.poll();
         }
@@ -53,59 +65,63 @@ public class MokshaPatam {
             path.add(location);
             location = pathTaken[location];
         }
+        System.out.println(path);
+        System.out.println(Arrays.deepToString(ladders));
+        System.out.println(Arrays.deepToString(snakes));
+        System.out.println("--------------------------------------------------------");
         return path.size();
     }
 
 
 
-    public static ArrayList<Integer> breadthFirstSearch(int location, ArrayList<Integer> visited, Queue<Integer> toVisit, int[] landmarks) {
-        if (visited.contains(location)) {
-            return null;
-        }
-        if (landmarks[location] == location && location != 0) {
-            return new ArrayList<>();
-        }
-        visited.add(location);
-        for (int i = 0; i < 6; i++) {
-            if (landmarks[location + i] != 0) {
-                toVisit.add(landmarks[location + 1]);
-            }
-            else {
-                toVisit.add(location + 1);
-            }
-        }
-    }
+//    public static ArrayList<Integer> breadthFirstSearch(int location, ArrayList<Integer> visited, Queue<Integer> toVisit, int[] landmarks) {
+//        if (visited.contains(location)) {
+//            return null;
+//        }
+//        if (landmarks[location] == location && location != 0) {
+//            return new ArrayList<>();
+//        }
+//        visited.add(location);
+//        for (int i = 0; i < 6; i++) {
+//            if (landmarks[location + i] != 0) {
+//                toVisit.add(landmarks[location + 1]);
+//            }
+//            else {
+//                toVisit.add(location + 1);
+//            }
+//        }
+//    }
 
-    public static ArrayList<Integer> bfs(int location, int parent, ArrayList<Integer> visited, Queue<Integer> toVisit, int[] landmarks) {
-        if (visited.contains(location)) {
-            return null;
-        }
-        if (landmarks[location] == location && location != 0) {
-            return new ArrayList<>();
-        }
-        for (int i = 0; i < 6; i++) {
-            if (visited.contains(location + i)) {
-                continue;
-            }
-            if (landmarks[location + i] != 0) {
-                toVisit.add(landmarks[location + i]);
-                visited.add(location + i);
-            }
-            else {
-                toVisit.add(location + i);
-                visited.add(location + 1)
-            }
-        }
-        bfs()
-    }
+//    public static ArrayList<Integer> bfs(int location, int parent, ArrayList<Integer> visited, Queue<Integer> toVisit, int[] landmarks) {
+//        if (visited.contains(location)) {
+//            return null;
+//        }
+//        if (landmarks[location] == location && location != 0) {
+//            return new ArrayList<>();
+//        }
+//        for (int i = 0; i < 6; i++) {
+//            if (visited.contains(location + i)) {
+//                continue;
+//            }
+//            if (landmarks[location + i] != 0) {
+//                toVisit.add(landmarks[location + i]);
+//                visited.add(location + i);
+//            }
+//            else {
+//                toVisit.add(location + i);
+//                visited.add(location + 1);
+//            }
+//        }
+//        bfs()
+//    }
 
-    public static int[] makeArray(int[][] arr1, int[][] arr2) {
-        int[] arr3 = new int[arr1.length + arr2.length];
+    public static int[] makeArray(int[][] arr1, int[][] arr2, int boardsize) {
+        int[] arr3 = new int[boardsize];
         for (int[] value : arr1) {
-            arr3[value[0]] = value[1];
+            arr3[value[0] - 1] = value[1] - 1;
         }
         for (int[] ints : arr2) {
-            arr3[ints[0]] = ints[1];
+            arr3[ints[0] - 1] = ints[1] - 1;
         }
         return arr3;
     }
